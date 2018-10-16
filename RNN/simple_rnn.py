262444b84534fd2896325f49a5897cfcd5ad4938
LynneZhou <lion19930924@163.com>
2018-10-17 00:17:30 +0800

    bptt

--- RNN/simple_rnn.py
+++ RNN/simple_rnn.py
@@ -39,11 +39,17 @@ class SimpleRNN(object):
         return np.argmax(o,axis=1)
 
     def __calculate_total_loss(self, x, y):
+        """
+        Calculate sentences loss.
+
+        Args:
+            x: sentences list, input sentence
+            y: sentences list, next word sentence
+        """
         L = 0
         for i in np.arange(len(y)):
             o,s=self.forward_propagation(x[i])
             correct_word_predictions=o[np.arange(len(y[i])),y[i]]  # prediction of correct words
-            print correct_word_predictions
             L+=-1*np.sum(np.log(correct_word_predictions))
         return L
 
@@ -51,13 +57,30 @@ class SimpleRNN(object):
         N = np.sum((len(y_i) for y_i in y))
         return self.__calculate_total_loss(x, y) / N
 
+    def bptt(self, x, y):
+        T = len(y)
+        o, s = self.forward_propagation(x)
+        dldU = np.zeros(self.U.shape)
+        dldV = np.zeros(self.V.shape)
+        dldW = np.zeros(self.W.shape)
+        delta_o = o
+        delta_o[np.arange(len(y)), y] -= 1
+        for t in np.arange(T)[::-1]:
+            dldV = np.outer(delta_o[t], s[t].T)
+            delta_t = self.V.T.dot(delta_o[t]) * (1 - (s[t] ** 2))
+            for bptt in np.arange(max(0, self._bptt_truncate), t + 1)[::-1]:
+                dldW += np.outer(delta_t, s[bptt - 1])
+                dldU[: ,x[bptt]] += delta_t
+                delta_t = self.W.T.dot(delta_t) * (1 - s[bptt-1] ** 2)
+        return dldU, dldV, dldW
+
 if __name__ == "__main__":
     file_path = "D:\\python script\\deep_learning\\DL_exercise\\DLExercise\\RNN\\data\\reddit.csv"
     preprocessor = PreProcessor()
     preprocessor.process_word_index(file_path)
-    X_train = preprocessor.X_train[:10]
-    Y_train = preprocessor.Y_train[:10]
+    X_train = preprocessor.X_train[10]
+    Y_train = preprocessor.Y_train[10]
     vocabulary_size = BaseConfig.vocabulary_size
     np.random.seed(10)
     rnn = SimpleRNN(vocabulary_size)
-    print rnn.calsulate_loss(X_train, Y_train)
+    print rnn.bptt(X_train, Y_train)
